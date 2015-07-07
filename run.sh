b8dd232f11eba5c0366aed1a9d1e7d96af97682b
ewantlf <ewantlf@gmail.com>
2015-07-07 15:48:12 +0300

    Typo fixed

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -dt --name rtorrent-rutorrent -p 8080:80 -p 443:443 -p 49160:49160/udp -p 49161:19161 -v ~/test/rtorrent:/downloads diameter/rtorrent-rutorrent:64
+docker run -dt --name rtorrent-rutorrent -p 8080:80 -p 443:443 -p 49160:49160/udp -p 49161:49161 -v ~/test/rtorrent:/downloads diameter/rtorrent-rutorrent:64
 
